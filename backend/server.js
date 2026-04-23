f3307faf8aad2b2382a630191a23f962026fa12c
Anshul Anand <mr.anshuking@gmail.com>
2026-04-23 11:12:16 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,22 +7,22 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['https://lost-found-1-6xic.onrender.com', 'http://localhost:5173'],
   credentials: true
+  
 }));
-app.use(express.json());
 
 
-
-// Routes
-app.use('/api', require('./routes/auth'));
-app.use('/api/items', require('./routes/items'));
 
 const dns = require('dns').promises;   // or just require('dns') in older Node
 dns.setServers(['8.8.8.8', '1.1.1.1']);   // to fix DNS refused block error
 
 
-// MongoDB Connect
+app.use(express.json());
+
+app.use('/api', require('./routes/auth'));
+app.use('/api/items', require('./routes/items'));
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
   .catch(err => console.log('❌ DB Error:', err));
